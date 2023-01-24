043c52b70cf369f8d308358484f74b1a858dae1a
Lars Hlmn <lars.hillmann@freenet.de>
2023-01-24 10:42:20 +0100

    HTML Button für Cntrl+Click

--- Variante2_detailed/longerCompare.js
+++ Variante2_detailed/longerCompare.js
@@ -1,25 +1,23 @@
+var LOCALSTORAGE_KEY = "PAGE_IDs";
+var DUPLICATE_ACTIVE = "DUPLICATE_ACTIVE";
+var pageIdValue = Math.floor(Math.random() * 3);
+var existingPageIds = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
 
-window.addEventListener('focus', checkForActiveTabs());
+bindUnload();
 
-    var LOCALSTORAGE_KEY = "PAGE_IDs";
-    var pageIdValue = Math.floor(Math.random() * 3);
-    var existingPageIds = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
+if(existingPageIds == null || existingPageIds === "NaN") {
+    existingPageIds = [];
+}
+
+console.log("Neue WindowId-Mock: " + pageIdValue + " \n Existing Window-Ids, die aus Local Storage gezogen wurden: " + existingPageIds);
     
-    bindUnload();
-    
-    
-    console.log("neue random: " + pageIdValue + " \n ExistingPages, die aus Local Storage gezogen wurden: " + existingPageIds)
-    
-    if(existingPageIds == null) {
-      existingPageIds = [];
-    } 
-        
-    if (existingPageIds.some(item => item == pageIdValue)) {
-        alert("Die Seite ist schonmal offen");
-        //Logik zum Fenster closen oder neue ID verteilen etc.
-    } else {
-        insertWindowIdToLocalStorage();
-    }
+if (existingPageIds.some(item => item ==  pageIdValue)) {
+    console.log("Window mit WindowId " + pageIdValue + " ist bereits offen.");
+    //Logik zum Fenster closen oder neue ID verteilaen etc.
+    //Flag setzen, dass wenn Fenster auf active ist und noch nicht verfügbar, dann schließen
+} else {
+    insertWindowIdToLocalStorage();
+}
 
 
 function insertWindowIdToLocalStorage() {
@@ -30,6 +28,8 @@ function insertWindowIdToLocalStorage() {
 
     //push to browser memory
     localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(existingPageIds)); 
+
+    console.log("Insert WIndow Id in LocalStorage erfolgreich. \nLocalStorage: " + localStorage.getItem(LOCALSTORAGE_KEY));
 }
 
 //kann womöglich raus
@@ -37,6 +37,7 @@ function checkForActiveTabs() {
     if (existingPageIds.some(item => item == pageIdValue)) {
         alert("Die Seite ist immernoch offen");
         //Logik zum Fenster closen oder neue ID verteilen etc.
+        //clear Flag der offenen Fenster
     } else {
         insertWindowIdToLocalStorage();
     }
@@ -46,8 +47,6 @@ function removeWindow() {
 
     var index = existingPageIds.indexOf(JSON.stringify(pageIdValue));
     
-    console.log("Index in Array: " + index + "\n für zahl: " + pageIdValue)
-
     if (index !== -1) {
         existingPageIds.splice(index, 1);
     }
